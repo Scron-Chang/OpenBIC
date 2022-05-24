124d46130273dc6c7be0bc06cb459e1222d846ac
matt <matt.lin@quantatw.com>
2022-05-24 11:07:59 +0800

    delete unuseful sensor address

--- meta-facebook/yv35-wf/src/platform/plat_sensor_table.h
+++ meta-facebook/yv35-wf/src/platform/plat_sensor_table.h
@@ -8,13 +8,6 @@
 #define VR_P0V9A_ASICA2_ADDR (0xB0 >> 1)
 #define SSD0_ADDR (0xD4 >> 1)
 #define SSD0_OFFSET 0x00
-#define  (0x92 >> 1)
-#define TMP75_ASIC_ADDR (0x98 >> 1)
-#define TMP75_TEMP_OFFSET 0x00
-//#define TMP_M24128_ADDR (0xA8 >> 1)
-//#define M24128_TEMP_OFFSET 0x00 //not check
-
-/*  define config for sensors  */
 #define TMP75_MB_ADDR (0x92 >> 1)
 #define TMP75_ASIC_ADDR (0x98 >> 1)
 #define ASIC_ADDR (0x00 >> 1) // TBU
