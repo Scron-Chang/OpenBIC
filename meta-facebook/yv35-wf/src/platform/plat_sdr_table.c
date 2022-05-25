f711e10edee839cc1afcaee3cfe0d7e0b5b1854a
matt <matt.lin@quantatw.com>
2022-05-25 11:11:19 +0800

    modify sdr name SENSOR_NUM_TEMP_CXL_CNTR

--- meta-facebook/yv35-wf/src/platform/plat_sdr_table.c
+++ meta-facebook/yv35-wf/src/platform/plat_sdr_table.c
@@ -507,7 +507,7 @@ SDR_Full_sensor plat_sdr_table[] = {
 
 		SELF_I2C_ADDRESS << 1, // owner id
 		0x00, // owner lun
-		SENSOR_NUM_TEMP_TMP75, // sensor number
+		SENSOR_NUM_TEMP_CXL_CNTR, // sensor number
 
 		IPMI_SDR_ENTITY_ID_SYS_BOARD, // entity id
 		0x00, // entity instance
