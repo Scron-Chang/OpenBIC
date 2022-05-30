512a4356a66ec3704da01294a722763cafaa4538
matt <matt.lin@quantatw.com>
2022-05-30 21:06:45 +0800

    adjust goip table and i2c device tree and add power control for FM_P12V_HSC_EN

--- meta-facebook/yv35-wf/src/platform/plat_init.c
+++ meta-facebook/yv35-wf/src/platform/plat_init.c
@@ -4,9 +4,16 @@
 #include "plat_class.h"
 #include "power_status.h"
 
+static void dev_power_init(void)
+{
+	gpio_set(FM_P12V_HSC_EN, gpio_get(FM_POWER_EN));
+}
+
 void pal_pre_init()
 {
 	init_platform_config();
+
+	dev_power_init();
 }
 
 void pal_set_sys_status()
